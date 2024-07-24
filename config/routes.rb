ecd9fa138bcde2100f68b49ed2b1a1e2228e80ee
duongngt-0538 <nguyen.gia.tung.duong@sun-asterisk.com>
2024-07-24 12:57:40 +0700

    [Naitei] Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    get "static_pages/home"
-    get "static_pages/help"
+    get "/", to: "static_pages#home"
+    get "/help", to: "static_pages#help"
 
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
     resources :users, only: %i(new create show)
+
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
 
     root "static_pages#home"
   end
